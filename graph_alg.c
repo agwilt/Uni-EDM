72283c43e795927b092076fbc32da381b6e29072
Andreas Gwilt <andreas@gwilt.net>
2017-11-28 12:08:59 +0100

    Start working on general edmonds-karp

--- graph_alg.c
+++ graph_alg.c
@@ -8,6 +8,8 @@
 
 #include "config.h"
 
+static double bfs_augm_path(struct graph *G, struct graph *G_back, double **f, int s, int t, int *prev);
+
 bool graph_bfs_path(struct graph *G, int s, int t, int *prev)
 {
 	prev[s] = -1;
@@ -43,6 +45,116 @@ end:
 	free(unexplored);
 
 	return exists_st_path;
+}
+
+static double bfs_augm_path(struct graph *G, struct graph *G_back, double **f, int s, int t, int *prev)
+{
+	prev[s] = -1;
+	char *visited = calloc(G->num_nodes, 1);
+	double *residual = calloc(G->num_nodes, sizeof(double));
+
+	int *q_next, *q_back, *unexplored;
+	q_next = q_back = unexplored = malloc(G->num_nodes * sizeof(int));
+	// q_back is next place to put new item
+	// q_next is next item to take
+
+	*(q_back++) = s; // push s
+	visited[s] = true;
+
+	while (q_next != q_back) {
+		int cur_node = *(q_next++);
+		for (int i=0; i<(G->nodes[cur_node].num_n); ++i) {
+			int neighbour_id = G->nodes[cur_node].neighbours[i].id;
+			// only consider node if edge isn't saturated
+			if (!visited[neighbour_id] && f[cur_node][neighbour_id] < G->nodes[cur_node].neighbours[i].weight) {
+				visited[neighbour_id] = true;
+				residual[neighbour_id] = G->nodes[cur_nodes].neighbours[i].weight - f[cur_node][neighbour_id];
+				prev[neighbour_id] = cur_node;
+				*(q_back++) = neighbour_id;
+				if (neighbour_id == t)
+					goto end;
+			}
+		}
+		for (int i=0; i<(G_back->nodes[cur_node].num_n); ++i) {
+			int neighbour_id = G_back->nodes[cur_node].neighbours[i].id;
+			// only consider edge if flow is positive
+			if (!visited[neighbour_id] && f[neighbour_id][cur_node] > 0) {
+				visited[neighbour_id] = true;
+				residual[neighbour_id] = f[neighbour_id][cur_node];
+				prev[neighbour_id] = cur_node;
+				*(q_back++) = neighbour_id;
+				if (neighbour_id == t)
+					goto end;
+			}
+		}
+	}
+
+end:
+	// now calculate val
+	double val;
+	if (visited[t]) {
+		val = HUGE_VAL;
+		for (int v=t; v!=s; v=prev[v])
+			if (residual[prev[v]][v] < val)
+				val = residual[prev[v]][v];
+	} else
+		val = 0;
+
+	free(visited);
+	free(unexplored);
+	free(residual);
+
+	return val;
+}
+
+double graph_edmonds_karp(struct *G, int s, int t)
+{
+	if (!G->is_directed) {
+		fprintf(stderr, "Error: Graph is undirected!\n");
+		exit(1);
+	}
+
+	// set initial flow to 0
+	struct graph f = {.num_nodes = 0, .max_nodes = 0, .nodes = NULL, .is_directed=true};
+	graph_add_nodes(&f, G->num_nodes);
+	for (int cur_node=0; current<G->num_nodes; cur_node++) {
+		for (int i=0; i<G->nodes[cur_node].num_n; ++i) {
+			graph_add_edge(&f, cur_node, G->nodes[cur_node].neighbours[i].id, 0.0);
+		}
+	}
+
+	// flow saved as 2-dim array, intialized to 0
+	double **f = malloc(G->num_nodes * sizeof(double *));
+	for (int i=0; i<G->num_nodes; ++i) {
+		f[i] = calloc(G->num_nodes * sizeof(double));
+	}
+
+	// new graph: G_back, where all the edges are the wrong way round
+	struct graph G_back = {.num_nodes=0, .max_nodes=0, .nodes=NULL, .is_directed=true};
+	graph_add_nodes(&G_back, G->num_nodes);
+	for (int cur_node=0; current<G->num_nodes; cur_node++) {
+		for (int i=0; i<G->nodes[cur_node].num_n; ++i) {
+			graph_add_edge(&f, G->nodes[cur_node].neighbours[i].id, cur_node, G->nodes[cur_node].neighbours[i].weight);
+		}
+	}
+
+	// save s-t-paths in prev
+	int *prev = malloc(G->num_nodes * sizeof(int));
+
+	double val;
+	while (val = bfs_augm_path(G, G_back, f, s, t, prev)) {
+		// prev is now s-t-path, val is value by which to augment
+		for (int v=t; v!=s; v=prev[v]) {
+			if (f[v][prev[v]] >= val)
+				f[v][prev[v]] -= val;
+			else
+				f[prev[v]][v] += val;
+		}
+	}
+	// now we have a max. s-t-flow
+
+	// now, separate out into cycles and paths
+	struct {int x; int y} start_edge = {0,0};
 }
 
 // prints list of edges to stdout
