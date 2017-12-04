3537ca8beb16afd1e0777411776c251702fdd702
Andreas Gwilt <andreas@gwilt.net>
2017-12-04 20:43:21 +0100

    Cheat around a bit with calloc

--- max-flow.c
+++ max-flow.c
@@ -31,18 +31,18 @@ int main(int argc, char *argv[])
 	/* silly idea: make dummy edge 0, fill everything with 0s */
 	int **edges = malloc(G.n * sizeof(int *));
 	for (int i=0; i<G.n; ++i) {
-		edges[i] = malloc(G.n * sizeof(int));
-		for (int j=0; j<G.n; ++j)
-			edges[i][j] = -1;
+		edges[i] = calloc(G.n, sizeof(int));
 	}
 
 	struct graph G_compr = {.n=0, .m=0, ._max_n=0, ._max_m=0, .E=NULL, .V=NULL};
+	G_compr.E = malloc(sizeof(struct edge));
+	G_compr.E[0] = (struct edge) {.id=0, .x=0, .y=0, .weight=0};
 	graph_add_nodes(&G_compr, G.n);
 	/* build new graph */
 	for (int e=0; e<G.m; ++e) {
 		int x = G.E[e].x;
 		int y = G.E[e].y;
-		if (edges[x][y] == -1) {	/* New edge */
+		if (edges[x][y] == 0) {	/* New edge */
 			edges[x][y] = graph_add_edge(&G_compr, x, y, G.E[e].weight);
 		} else {
 			G_compr.E[edges[x][y]].weight += G.E[e].weight;
@@ -60,14 +60,14 @@ int main(int argc, char *argv[])
 	for (int e=0; e<G.m; ++e) {
 		int x = G_compr.E[e].x;
 		int y = G_compr.E[e].y;
-		/* set to -1 if weight == 0 */
-		if (edges[x][y] != -1) {
+		/* set to 0 if weight == 0 */
+		if (edges[x][y] != 0) {
 			if (G_compr.E[edges[x][y]].weight > G.E[e].weight) {
 				f[e] = G.E[e].weight;
 				G_compr.E[edges[x][y]].weight -= f[e];
 			} else {
 				f[e] = G_compr.E[edges[x][y]].weight;
-				edges[x][y] = -1;
+				edges[x][y] = 0;
 			}
 		}
 	}
